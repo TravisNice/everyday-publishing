218ed2873a57ced66b3a285ab872964f42211282
Travis Nice <travis.nice@everydaypublishing.com.au>
2017-08-23 14:03:53 +1000

    Removed WooCommerce from menu-top.php

--- template-parts/menu-top.php
+++ template-parts/menu-top.php
@@ -36,8 +36,6 @@
 			    'items_wrap' => '%3$s',
 			    'item_spacing' => 'preserve'
 			    );
-	
-	global $woocommerce;
 ?>
 
 <div class="ep-top">
@@ -53,7 +51,6 @@
 			Everyday Publishing
 		</a>
 		<?php wp_nav_menu ( $largeArgs ); ?>
-		<a class="ep-button ep-bar-item ep-hide-small ep-right" href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><i class="fa fa-shopping-basket ep-margin-right" aria-hidden="true"></i><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?> - <?php echo $woocommerce->cart->get_cart_total(); ?></a>
 	</div>
 
 	<!-- Small Screens -->
