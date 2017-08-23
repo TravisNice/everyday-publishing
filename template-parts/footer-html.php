2efecd3a23df0c427998a7143bb865d7e1cafe6f
Travis Nice <travis.nice@everydaypublishing.com.au>
2017-08-23 13:54:54 +1000

    Removed "My Account" from the footer

--- template-parts/footer-html.php
+++ template-parts/footer-html.php
@@ -4,7 +4,7 @@
 	<a class="ep-button ep-large ep-light-brass" href="https://www.facebook.com/everydaypublishingaustralia/" title="Facebook"><i class="fa fa-facebook"></i></a>
 	<a class="ep-button ep-large ep-light-brass" href="https://twitter.com/QueryEveryday" title="Twitter"><i class="fa fa-twitter"></i></a>
 	<p>Copyright &copy; <?php echo date('Y'); ?> Everyday Publishing. All Rights Reserved.</p>
-	<p><a href="/terms-conditions">Terms &amp; Conditions</a> | <a href="/privacy-policy">Privacy Policy</a> | <a href="/my-account">My Account</a></p>
+	<p><a href="/terms-conditions">Terms &amp; Conditions</a> | <a href="/privacy-policy">Privacy Policy</a></p>
 </footer>
 
 <?php wp_footer(); ?>
