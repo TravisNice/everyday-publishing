7fd07236241de15902890bb245cd77a6edda5b76
Travis Nice <travis.nice@everydaypublishing.com.au>
2017-09-28 03:27:22 +1000

    fixed bug where I left in localhost on production machine

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 	<small><a href="tel:+61746714496">(07) 4671 4496</a></small>
 </footer>
 
-<script type="text/javascript" src="http://localhost:8888/wp-content/themes/everyday-publishing/includes/toggle.js?ver=4.8.2" async="async"></script>
+<script type="text/javascript" src="https://www.everydaypublishing.com.au/wp-content/themes/everyday-publishing/includes/toggle.js?ver=4.8.2" async="async"></script>
 <link rel="stylesheet" id="font-awesome-css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css?ver=4.8.2" async="async" type="text/css" media="all">
-<link rel="stylesheet" id="everyday-publishing-css" href="http://localhost:8888/wp-content/themes/everyday-publishing/style.css?ver=4.8.2" async="async" type="text/css" media="all">
+<link rel="stylesheet" id="everyday-publishing-css" href="https://www.everydaypublishing.com.au/wp-content/themes/everyday-publishing/style.css?ver=4.8.2" async="async" type="text/css" media="all">
 <?php wp_footer(); ?>
 
 </body>
