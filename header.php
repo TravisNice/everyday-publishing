b093c06e59bf8fd493bfbee1c9a0614518ebfbdc
Travis Nice <travis.nice@everydaypublishing.com.au>
2017-09-28 21:27:50 +1000

    fixed bug where I left http instead of https in last change

--- header.php
+++ header.php
@@ -27,9 +27,9 @@ touch with us through our contact page. -->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel='stylesheet' id='everyday-publishing-css'  href='http://www.everydaypublishing.com.au/wp-content/themes/everyday-publishing/style.css?ver=4.8.2' async type='text/css' media='all' />
+<link rel='stylesheet' id='everyday-publishing-css'  href='https://www.everydaypublishing.com.au/wp-content/themes/everyday-publishing/style.css?ver=4.8.2' async type='text/css' media='all' />
 <link rel='stylesheet' id='font-awesome-css'  href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css?ver=4.8.2' async type='text/css' media='all' />
-<script type='text/javascript' src='http://www.everydaypublishing.com.au/wp-content/themes/everyday-publishing/includes/toggle.js?ver=4.8.2' async></script>
+<script type='text/javascript' src='https://www.everydaypublishing.com.au/wp-content/themes/everyday-publishing/includes/toggle.js?ver=4.8.2' async></script>
 <?php wp_head(); ?>
 
 </head>
