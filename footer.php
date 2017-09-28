8645b465c88764ef3013a04895bb7b86ec5ecdf0
Travis Nice <travis.nice@everydaypublishing.com.au>
2017-09-29 03:26:59 +1000

    moved the render blocking css and js into the footer
    even though it is set to async like Google wants the cunts still bitch about it.

--- footer.php
+++ footer.php
@@ -7,6 +7,9 @@
 			</small>
 		</footer>
 
+		<link rel='stylesheet' id='everyday-publishing-css'  href='/wp-content/themes/everyday-publishing/style.css?ver=4.8.2' async type='text/css' media='all' />
+		<link rel='stylesheet' id='font-awesome-css'  href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css?ver=4.8.2' async type='text/css' media='all' />
+		<script type='text/javascript' src='/wp-content/themes/everyday-publishing/includes/toggle.js?ver=4.8.2' async></script>
 		<?php wp_footer(); ?>
 
 		</body>
--- header.php
+++ header.php
@@ -3,9 +3,6 @@
 		<head>
 			<meta charset="<?php bloginfo( 'charset' ); ?>" />
 			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<link rel='stylesheet' id='everyday-publishing-css'  href='/wp-content/themes/everyday-publishing/style.css?ver=4.8.2' async type='text/css' media='all' />
-			<link rel='stylesheet' id='font-awesome-css'  href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css?ver=4.8.2' async type='text/css' media='all' />
-			<script type='text/javascript' src='/wp-content/themes/everyday-publishing/includes/toggle.js?ver=4.8.2' async></script>
 			<?php wp_head(); ?>
 		</head>
 
